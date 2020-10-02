965fc6419307316e966fa52782e17b28d3b86215
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-10-02 15:13:29 -0300

    Ajutar o APP

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -17,70 +17,76 @@ class _HomePageState extends State<HomePage> {
   int _quantade = 0;
 
   @override
-  void initState() {
-    super.initState();
-    _getGIFs().then((value) {});
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Image.network(
-            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
-        centerTitle: true,
-      ),
+      appBar: _appBar(),
       backgroundColor: Colors.black,
       body: Column(
         children: <Widget>[
           Padding(
             padding: EdgeInsets.all(15.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  labelText: "Pesquisar..",
-                  labelStyle: TextStyle(
-                    color: Colors.white,
-                  ),
-                  border: OutlineInputBorder()),
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 18.0, color: Colors.white),
-              onSubmitted: (text) {
-                setState(() {
-                  _pesquisa = text;
-                  _quantade = 0;
-                });
-              },
-            ),
+            child: _textFildPesquisa(),
           ),
           Expanded(
-            child: FutureBuilder(
-              future: _getGIFs(),
-              // ignore: missing_return
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
-                  case ConnectionState.none:
-                    return Container(
-                      width: 200.0,
-                      height: 200.0,
-                      alignment: Alignment.center,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                        strokeWidth: 5.0,
-                      ),
-                    );
-                  default:
-                    if (snapshot.hasError)
-                      return Container();
-                    else
-                      return _criarTabelaGIFs(context, snapshot);
-                }
-              },
-            ),
+            child: _futureBuilder(),
           )
         ],
       ),
+    );
+  }
+
+  Widget _appBar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      title: Image.network(
+          "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _textFildPesquisa() {
+    return TextField(
+      decoration: InputDecoration(
+          labelText: "Pesquisar..",
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+          border: OutlineInputBorder()),
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 18.0, color: Colors.white),
+      onSubmitted: (text) {
+        setState(() {
+          _pesquisa = text;
+          _quantade = 0;
+        });
+      },
+    );
+  }
+
+  Widget _futureBuilder() {
+    return FutureBuilder(
+      future: _getGIFs(),
+      // ignore: missing_return
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+          case ConnectionState.none:
+            return Container(
+              width: 200.0,
+              height: 200.0,
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                strokeWidth: 5.0,
+              ),
+            );
+          default:
+            if (snapshot.hasError)
+              return Container();
+            else
+              return _criarTabelaGIFs(context, snapshot);
+        }
+      },
     );
   }
 
