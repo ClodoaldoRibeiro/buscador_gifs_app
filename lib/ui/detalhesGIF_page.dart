891f5655c13eb0a2c9cfd6f5929414018350141a
Clodoaldo Ribeiro <clodoaldorib@hotmail.com>
2020-10-13 11:01:38 -0300

    Ajustes

--- lib/ui/detalhesGIF_page.dart
+++ lib/ui/detalhesGIF_page.dart
@@ -9,21 +9,20 @@ class GifPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(_gifData["title"]),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.share),
-            onPressed: () {
-              Share.share(_gifData["images"]["fixed_height"]["url"]);
-            },
-          )
-        ],
-      ),
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Image.network(_gifData["images"]["fixed_height"]["url"]),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(_gifData["title"]),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {
+                Share.share(_gifData["images"]["fixed_height"]["url"]);
+              },
+            )
+          ],
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Image.network(_gifData["images"]["fixed_height"]["url"]),
+        ));
   }
 }
